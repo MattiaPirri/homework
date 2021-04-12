8561a07df9e7d3be8b04f884876a0e84bf0a6665
MattiaPirri <79869589+MattiaPirri@users.noreply.github.com>
2021-04-12 12:16:06 +0200

    Add files via upload

---
+++ mhw2/contents.js
@@ -0,0 +1,37 @@
+const contents = [{
+        marca: 'Sony',
+        nome: 'α7R IV',
+        immagine: 'https://s3.eu-central-1.amazonaws.com/static2.euronics.it/immagini/img-catalogo-converted/full/182025407.jpg',
+        stelle: 3,
+        recensioni: 3,
+        prezzo: 3799.9,
+        descrizione: 'α7R IV, mirrorless full-frame, offre un’esperienza fotografica eccezionale, racchiudendo in un corpo compatto l’abilità espressiva tipica delle fotocamere di medio formato e un’eccezionale velocità. La rigidità ottimizzata e le opzioni di connettività assicurano una produttività senza pari.'
+    },
+    {
+        marca: 'Pavesi',
+        nome: 'Gocciole',
+        immagine: 'https://images-na.ssl-images-amazon.com/images/I/71ZYg36b-FL._AC_SL1500_.jpg',
+        stelle: 4,
+        recensioni: 7,
+        prezzo: 3.9,
+        descrizione: 'Ingredienti: Farina di frumento, Zucchero, Cioccolato 14,6% (zucchero, pasta di cacao, burro di cacao, cacao magro, emulsionante: lecitina di soia), Olio di girasole, Burro, Sciroppo di glucosio, Amido di frumento, Agenti lievitanti (carbonato acido di sodio, carbonato acido d\'ammonio), Sale, Aroma vanillina, Può contenere tracce di: frutta a guscio, sesamo e uova.'
+    },
+    {
+        marca: 'Quasar',
+        nome: 'Vetri 650ml',
+        immagine: 'https://images-na.ssl-images-amazon.com/images/I/61lsmzZtwNL._AC_SL1500_.jpg',
+        stelle: 4,
+        recensioni: 3,
+        prezzo: 2.99,
+        descrizione: 'VETRI FORMULA ORIGINALE: Quasar Vetri rimuove efficacemente lo sporco facendo brillare e scintillare vetri, cristalli e specchi senza lasciare tracce e aloni. Asciuga rapidamente ed è ottimo anche per la detersione di tutte le superfici lavabili come: piastrelle, plastica, acciaio inox, cromature, lampadari. Ideale anche per il marmo. '
+    },
+    {
+        marca: 'Quasar',
+        nome: 'Legno 650ml',
+        immagine: 'https://www.quasardetergenti.it/sites/default/files/styles/prodotto/public/Legno-Spray_0.png?itok=3pMmc6rw',
+        stelle: 5,
+        recensioni: 3,
+        prezzo: 2.99,
+        descrizione: 'Quasar Legno pulisce efficacemente le piccole superfici in legno come mobili e porte facendole brillare senza lasciare tracce e aloni. Grazie alla sua formula arricchita con cera d\'api, assicura un effetto protettivo e antipolvere.'
+    }
+];
